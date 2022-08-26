e7ee7561b7850bf233279acff19fb4c66e2d423a
Kirill Kainov <kainov.dev@gmail.com>
2022-08-26 19:40:15 +0300

    add task2

---
+++ src/Main2.java
@@ -0,0 +1,30 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 6, 3, 2, 5, 1, 4, 9};
+        System.out.println("Before:");
+        println(arr);
+        bubbleSort(arr);
+        System.out.println("After:");
+        println(arr);
+    }
+
+    private static void println(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    private static void bubbleSort(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+    }
+}
